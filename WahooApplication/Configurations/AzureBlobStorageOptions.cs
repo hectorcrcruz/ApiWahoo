a1842795c88c9d738dab51a4384072e880dce8d1
hectorcrcruz <hectorcrcruz98@outlook.com>
2025-07-30 14:47:24 -0500

    Delete WahooApplication/Configurations directory

--- WahooApplication/Configurations/AzureBlobStorageOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WahooApplication.Commons
-{
-    public class AzureBlobStorageOptions
-    {
-        public string ConnectionString { get; set; }
-        public string ContainerName { get; set; }
-    }
-
-}
